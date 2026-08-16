db565f5471a20809c37e205a401aabebccd8b2e8
lefpap <lolopap21@gmail.com>
2026-08-16 16:49:52 +0300

    initial commit

---
+++ src/main/java/io/github/lefpap/briefly/BrieflyApplication.java
@@ -0,0 +1,13 @@
+package io.github.lefpap.briefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrieflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrieflyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/lefpap/briefly/BrieflyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lefpap.briefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrieflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
